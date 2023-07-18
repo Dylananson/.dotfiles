10c7adb83fb5b7cd411b6fea599549a5b7f15a9c
dylananson <dylananson144@gmail.com>
2023-07-18 10:13:21 +1000

    formatting works now

--- nvim/.config/nvim/lua/plugins/eslint.lua
+++ nvim/.config/nvim/lua/plugins/eslint.lua
@@ -1,30 +1,27 @@
-
 return {
 	"MunifTanjim/eslint.nvim",
-    cmd = "ESLint",
-    lazy = false,
-    config = function()
-    local eslint = require("eslint")
-
-
-    eslint.setup({
-      bin = 'eslint_d', -- or `eslint_d`
-      code_actions = {
-        enable = true,
-        apply_on_save = {
-          enable = false,
-          types = { "problem" }, -- "directive", "problem", "suggestion", "layout"
-        },
-        disable_rule_comment = {
-          enable = true,
-          location = "separate_line", -- or `same_line`
-        },
-      },
-      diagnostics = {
-        enable = true,
-        report_unused_disable_directives = false,
-        run_on = "type", -- or `save`
-      },
-    })
-    end
+	cmd = "ESLint",
+	lazy = false,
+	config = function()
+		local eslint = require("eslint")
+		eslint.setup({
+			bin = "eslint_d", -- or `eslint_d`
+			code_actions = {
+				enable = true,
+				apply_on_save = {
+					enable = false,
+					types = { "problem" }, -- "directive", "problem", "suggestion", "layout"
+				},
+				disable_rule_comment = {
+					enable = true,
+					location = "separate_line", -- or `same_line`
+				},
+			},
+			diagnostics = {
+				enable = true,
+				report_unused_disable_directives = false,
+				run_on = "type", -- or `save`
+			},
+		})
+	end,
 }
