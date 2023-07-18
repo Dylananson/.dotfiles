4cf34daa92c4528d0a091ee08c74884c3fa44dcb
dylananson <dylananson144@gmail.com>
2023-07-18 10:23:45 +1000

    change comment

---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,22 @@
+return {
+	-- Comments
+	"echasnovski/mini.comment",
+	event = "VeryLazy",
+	dependencies = {
+		{ "JoosepAlviste/nvim-ts-context-commentstring", event = "VeryLazy", lazy = true },
+	},
+	opts = {
+		hooks = {
+			pre = function()
+				require("ts_context_commentstring.internal").update_commentstring({})
+			end,
+		},
+		mappings = {
+			comment = "<C-_>",
+			comment_line = "<C-_>",
+		},
+	},
+	config = function(_, opts)
+		require("mini.comment").setup(opts)
+	end,
+}
