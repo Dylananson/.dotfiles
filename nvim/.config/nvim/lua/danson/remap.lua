5577329e9dd2cdd9970597d184e003664060c8ae
Dylan Anson <dylan.anson@carsales.com.au>
2024-02-28 14:40:17 +1100

    remaps

--- nvim/.config/nvim/lua/danson/remap.lua
+++ nvim/.config/nvim/lua/danson/remap.lua
@@ -7,3 +7,8 @@ nnoremap("<C-d>", "<C-d>zz")
 nnoremap("<C-u>", "<C-u>zz")
 nnoremap("n", "nzzzv")
 nnoremap("N", "Nzzzv")
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader><C-n>", "<cmd>silent !tmux neww tmux-sessionator ~/code/notes<CR>")
+vim.keymap.set("n", "<leader>D", "<cmd>silent !tmux neww tmux-sessionator ~/.dotfiles<CR>")
+
