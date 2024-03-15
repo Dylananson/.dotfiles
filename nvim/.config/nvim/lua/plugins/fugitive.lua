1319587676e3cd6589db772be8e16cd477699044
Dylan <dylananson144@gmail.com>
2024-03-16 07:22:28 +1100

    changes

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,30 +1,25 @@
 return {
 	"tpope/vim-fugitive",
 	config = function(_, _)
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
 
-		local bufnr = vim.api.nvim_get_current_buf()
-
-		local map = function(keys, func, desc)
-			vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-		end
-		local opts = { buffer = bufnr, remap = false }
-
-		-- vim.keymap.set("n", "<leader>gp", function()
-		-- vim.cmd.Git("push")
-		-- end, opts, "[G]it [P]ush")
-
-		map("<leader>gp", function()
+		vim.keymap.set("n", "<leader>gp", function()
 			vim.cmd.Git({ "push" })
-		end, "[G]it [P]ush ")
+		end, { remap = false, desc = "[G]it [P]ush " })
+
+		--for some reason the maps dont load without this print...
+		vim.keymap.set("n", "<leader>gp", function()
+			vim.cmd.Git({ "push" })
+		end, { remap = false, desc = "[G]it [P]ush " })
 
 		-- rebase always
-		map("<leader>gP", function()
+		vim.keymap.set("n", "<leader>gP", function()
 			vim.cmd.Git({ "pull", "--rebase" })
-		end, "[G]it [P]ush ")
+		end, { remap = false, desc = "[G]it [P]ull " })
 
 		-- NOTE: It allows me to easily set the branch i am pushing and any tracking
 		-- needed if i did not set the branch up correctly
-		map("<leader>gt", ":Git push -u origin", "[G]it [T]rack and push")
+
+		vim.keymap.set("n", "<leader>gt", ":Git push -u origin", { remap = false, desc = "[G]it [T]rack and push" })
 	end,
 }
